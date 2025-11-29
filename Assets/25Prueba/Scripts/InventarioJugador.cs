7ea8d100fd38b0ffc0c5b6019eb1d742049c713d
IvanRuso <ivansoto64@aragon.unam.mx>
2025-11-28 22:53:31 -0600

    CHINGE SU MADRE AHORA SI

--- Assets/25Prueba/Scripts/InventarioJugador.cs
+++ Assets/25Prueba/Scripts/InventarioJugador.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class InventarioJugador : MonoBehaviour
 {
